ac57847ab5a63aa3397d59c739b29bbd6975170e
Mishkat Eltayeb <129929931+Mkat-Sl@users.noreply.github.com>
2024-07-14 23:09:43 +0300

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,6 +1,8 @@
 function checkAnswer(){
     const correctAnswer = "4";
-    const userAnswer = document.querySelector('input[type = "radio"]:checked').value;
+    // const userAnswer = document.querySelector('input[type = "radio"]:checked').value;
+    const userAnswer = document.querySelector('input[name="quiz"]:checked').value;
+
     const feedBack = document.getElementById('feedback');
 
     if(userAnswer === correctAnswer){
