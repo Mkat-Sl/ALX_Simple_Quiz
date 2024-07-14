bb431d30a3203ead5db045ac4a09befa32e600c1
Mishkat <mishkatsuliman101@gmail.com>
2024-07-14 00:26:15 -0700

    Js File Update

---
+++ ALX_Simple_Quiz-main/quiz.js
@@ -0,0 +1,15 @@
+function checkAnswer(){
+    const correctAnswer = "4";
+    const userAnswer = document.querySelector('input[type = "radio"]:checked').value;
+    const feedBack = document.getElementById('feedback');
+
+    if(userAnswer === correctAnswer){
+        feedBack.textContent = "Correct! Well done.";
+    } else {
+        feedBack.textContent = "That's incorrect. Try again!";
+    }
+};
+const submitBtn = document.getElementById('submit-answer');
+submitBtn.addEventListener('click', checkAnswer);
+
+
