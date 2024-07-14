e9ec155889442153074d90a6c931db5c1209c971
Mishkat Eltayeb <129929931+Mkat-Sl@users.noreply.github.com>
2024-07-14 00:27:38 -0700

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,27 +1,15 @@
-// const answers = document.querySelectorAll('input');
-// Array.from(answers).forEach(function(answer){
-//     answer.addEventListener('checked', function(){
-//         const correctAnswer = 
+function checkAnswer(){
+    const correctAnswer = "4";
+    const userAnswer = document.querySelector('input[type = "radio"]:checked').value;
+    const feedBack = document.getElementById('feedback');
 
-//     })
-// })
-const quizes = document.querySelectorAll('name');
-// Array.from(quizes).forEach(function(e){
-// });
-function checkAnswer(correctAnswer){
-    correctAnswer = 4;
-    const userAnswer = quizes.querySelectorAll('input[type = "radio"]').value;
-    const feedBack = document.querySelector('#feedback');
-    if(correctAnswer = userAnswer){
-        feedBack.textContent = 'Correct! Well done.';
-        console.log(feedBack);
+    if(userAnswer === correctAnswer){
+        feedBack.textContent = "Correct! Well done.";
     } else {
-        feedBack.textContent = 'hjsj';
-        console.log(feedBack);
+        feedBack.textContent = "That's incorrect. Try again!";
+    }
+};
+const submitBtn = document.getElementById('submit-answer');
+submitBtn.addEventListener('click', checkAnswer);
 
-    };
-    const submitBtn = document.getElementById('#submit-answer');
-    submitBtn.addEventListener('click', checkAnswer);
-    // const userAnswer = document.querySelectorAll('')
-    // const userAnswer = quize.querySelector
-}
+
